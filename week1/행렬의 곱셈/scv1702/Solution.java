131a89239c5f6a204394ce5f96b1fedcb82f2802
신찬규(Shin Changyu) <63400830+scv1702@users.noreply.github.com>
2024-01-03 15:45:17 +0900

    solve: week1-행렬의 곱셈

--- week1/행렬의 곱셈/scv1702/Solution.java
+++ week1/행렬의 곱셈/scv1702/Solution.java
@@ -1,13 +1,3 @@
-# 문제 풀이
-
-## 문제 해설
-행렬의 곱셈 정의에 따라 동일하게 구현하면 된다.
-
-## 시간 복잡도
-
-`arr1` 행렬을 `m x k`, `arr2` 행렬을 `k x n`이라고 했을 때 행렬의 곱셈 시간 복잡도는 `O(mnk)`이다.
-
-```java
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         int m = arr1.length;
@@ -24,4 +14,3 @@ class Solution {
         return answer;
     }
 }
-```
