9f1c885cd11953c822bf2d3f24af3f4d56984b05
Sanghee0820 <lsh277604@gmail.com>
2024-01-12 18:40:05 +0900

    solve: week2-압축

--- week2/[3차] 압축/sanghee0820/Solution.java
+++ week2/[3차] 압축/sanghee0820/Solution.java
@@ -1,25 +1,17 @@
-# 문제 풀이
-
-## 문제 해설
-1. HashMap 에 A ~ Z의 값을 저장
-2. msg를 순회하며 subMsg가 key에 포함되어있을 경우 return에 값 추가.
-
-## 시간 복잡도
-
-1. HashMap에 A ~ Z의 값을 저장 O(26);
-```java
-public void initSecretCode(){
+import java.util.*;
+class Solution {
+    public HashMap<String, Integer> secretCode;
+    public Integer valueCnt;
+    
+    public void initSecretCode(){
         secretCode = new HashMap<>();
         for(char code = 'A'; code <= 'Z'; code++){
             secretCode.put(String.valueOf(code), code - 'A' + 1);
         }
         valueCnt = 26;
     }
-```
-
-2. msg를 순회하며 subMsg 확인 O(n) n = msg의 길이
-```java
-public List<Integer> toSecretCode(String msg){
+    
+    public List<Integer> toSecretCode(String msg){
         List<Integer> secret = new ArrayList<>();
 
         for(int index = 1; index-1 < msg.length(); ){
@@ -48,12 +40,9 @@ public List<Integer> toSecretCode(String msg){
         
         return secret;
     }
-```
-
-## 결과
-
-> 최초 secretCode Map을 만드는데 A~Z의 값을 넣으므로 O(26)의 시간이 걸린다.
->
-> 또 msg를 순회하며 subMsg를 만들고 secretCode에 추가하는 로직은 msg를 순회하는데 걸리는 시간과 비슷하므로
-> 
-> O(n)이 걸린다.
+    public List<Integer> solution(String msg) {
+        initSecretCode();
+        List<Integer> test = toSecretCode(msg);
+        return test;
+    }
+}
