eab3ea3d0c5f7a7d86b15220fb21f47f509e7b48
Shin Changyu <scv1702@naver.com>
2024-01-15 19:44:58 +0900

    solve: week3-뒤에 있는 큰 수 찾기

---
+++ week3/뒤에 있는 큰 수 찾기/scv1702/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int n = numbers.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+    
+        stack.push(0);
+        
+        for (int i = 1; i < n; i++) {
+            while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
+                answer[stack.pop()] = numbers[i];
+            }
+            stack.push(i);
+        }
+        
+        while (!stack.isEmpty()) {
+            answer[stack.pop()] = -1;
+        }
+        
+        return answer;
+    }
+}
