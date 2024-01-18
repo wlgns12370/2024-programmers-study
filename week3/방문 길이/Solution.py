e4799f8bf45d6257375ada9bdaf3765cdad12212
김진현 <79571893+kimjhyun0627@users.noreply.github.com>
2024-01-18 08:13:25 +0000

    solve: week3 - 방문 길이

---
+++ week3/방문 길이/Solution.py
@@ -0,0 +1,21 @@
+def solution(dirs):
+
+    direction = {'U' : (-1, 0), 'D' : (1, 0), 'L' : (0, -1), 'R' : (0, 1)}
+    passed = set()
+    
+    x, y = 0, 0
+    for d in dirs:
+        new_x = x + direction[d][0]
+        new_y = y + direction[d][1]
+        if -5 <= new_x <= 5 and -5 <= new_y <= 5:
+            passed.add(((x, y), (new_x, new_y)))
+            passed.add(((new_x, new_y), (x, y)))
+            x = new_x
+            y = new_y
+
+    answer = len(passed) // 2
+    return answer
+
+
+print(solution("ULURRDLLU"))
+print(solution("LULLLLLLU"))
