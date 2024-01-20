629f7f085a658f07399dbf1747406e35ac996d6e
신찬규(Shin Changyu) <63400830+scv1702@users.noreply.github.com>
2024-01-20 20:37:09 +0900

    Revert "solve: week3"

--- week3/땅따먹기/Solution.py
+++
@@ -1,7 +0,0 @@
-def solution(land):
-    for ii in range(1, len(land)):
-        for jj in range(4):
-            land[ii][jj] += max(land[ii-1][:jj] + land[ii-1][jj+1:])
-        
-    answer = max(land[len(land)-1])
-    return answer
